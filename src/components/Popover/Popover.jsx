f05ba9eddccd9fa7833fb79a553e15ce1a2a24d3
20120472 <hienthai.030102@gmail.com>
2024-05-06 16:24:45 +0700

    add event

--- src/components/Popover/Popover.jsx
+++ src/components/Popover/Popover.jsx
@@ -1,27 +1,36 @@
 import Tippy, { tippy } from '@tippyjs/react';
 import { getChildByType } from 'react-nanny';
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Prevent useEffect to run twice in dev mode
 let lock = 0;
 
-function Popover({ placement = 'bottom-start', offset = [15, 0], autoClose, children }) {
+function Popover({
+  placement = 'bottom-start',
+  offset = [15, 0],
+  onHide,
+  onShow,
+  autoClose,
+  children,
+}) {
   const handlerRef = useRef(null);
 
   const handler = getChildByType(children, ['PopoverHandler']);
   const content = getChildByType(children, ['PopoverContent']);
 
-  const handleScroll = (e) => {
+  const handleScroll = useCallback((e) => {
     handlerRef.current.click();
-  };
+  }, []);
 
   function handleShow() {
+    if (onShow) onShow();
     if (!autoClose) return;
     document.getElementById('main').addEventListener('scroll', handleScroll);
   }
 
   function handleHide() {
+    if (onHide) onHide();
     if (!autoClose) return;
     document.getElementById('main').removeEventListener('scroll', handleScroll);
   }
